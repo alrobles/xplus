6759de3a4b6da035d85543c1043042f91100fc99
Angel Luis Robles Fernández <a.l.robles.fernandez@gmail.com>
2026-04-19 12:43:27 -0500

    Remove reference from fit_xplus_example documentation
    Removed reference to Robles et al. (2022) from documentation.

--- R/fit_xplus_example.R
+++ R/fit_xplus_example.R
@@ -3,5 +3,4 @@
 #' Example model fit used in package documentation.
 #'
 #' @format An object of class `xplus`.
-#' @references Robles et al. (2022). doi:10.1371/journal.pcbi.1009956
 "fit_xplus_example"
