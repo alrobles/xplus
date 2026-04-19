775939f64f2e3a42f1487791b32e16fa89e704da
Angel Luis Robles Fernández <a.l.robles.fernandez@gmail.com>
2026-04-19 12:38:53 -0500

    Update reference in binexample.R dataset documentation

--- R/binexample.R
+++ R/binexample.R
@@ -3,5 +3,5 @@
 #' A small binary example dataset used in package examples.
 #'
 #' @format A data frame.
-#' @references Robles et al. (2022). doi:10.1371/journal.pcbi.1009956
+#' @references Zhu et al. (2022). doi:10.1371/journal.pcbi.1009956
 "binexample"
