28cffea01b995fb1000fdf4b5992516899234474
Angel Luis Robles Fernández <a.l.robles.fernandez@gmail.com>
2026-04-19 12:45:58 -0500

    Remove reference from xplus object example documentation
    Remove reference to Robles et al. (2022) from documentation.

--- R/xplus_object_example.R
+++ R/xplus_object_example.R
@@ -3,5 +3,4 @@
 #' Saved xplus object for examples and tests.
 #'
 #' @format An object of class `xplus`.
-#' @references Robles et al. (2022). doi:10.1371/journal.pcbi.1009956
 "xplus_object_example"
